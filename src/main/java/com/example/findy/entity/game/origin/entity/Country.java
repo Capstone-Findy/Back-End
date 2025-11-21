f0c58d000c326f9092acfb704c1126f05a9b2271
한승민 <ehrehrdltkfk@gmail.com>
2025-11-22 02:07:21 +0900

    fix : add country

--- src/main/java/com/example/findy/entity/game/origin/entity/Country.java
+++ src/main/java/com/example/findy/entity/game/origin/entity/Country.java
@@ -5,5 +5,13 @@ import com.findy.processor.RestDocs;
 @RestDocs("국가")
 public enum Country {
     @RestDocs("대한민국")
-    SouthKorea
+    Korea,
+    @RestDocs("중국")
+    China,
+    @RestDocs("프랑스")
+    France,
+    @RestDocs("일본")
+    Japan,
+    @RestDocs("미국")
+    USA
 }
